26271fe970b21b63dbb9c1bd7d3a3d6df541c427
James Sullivan <james_sullivan@mckinsey.com>
2018-10-03 11:12:06 -0600

    Add env vars for github oauth

--- server/util/config.js
+++ server/util/config.js
@@ -1,3 +1,8 @@
 module.exports = {
   port: process.env.PORT || 8080,
-}
+  nodeEnv: process.env.NODE_ENV || 'production',
+  clientId: process.env.OAUTH_CLIENT_ID || '',
+  clientSecret: process.env.OAUTH_CLIENT_SECRET || '',
+  redirectUrl: process.env.REDIRECT_URL || '',
+  gitHostname: process.env.GIT_HOSTNAME || '',
+};
