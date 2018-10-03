9ebb13dd0cde0116bb197d2ddf3e46bfcca2e18a
James Sullivan <james_sullivan@mckinsey.com>
2018-10-03 14:09:04 -0600

    Movre auth business logic to service class

---
+++ server/services/auth.service.js
@@ -0,0 +1,86 @@
+const simpleOauthModule = require('simple-oauth2');
+const randomstring = require('randomstring');
+
+const logger = require('../util/logger');
+const config = require('../util/config');
+
+class AuthService {
+  constructor() {
+    this.oauth_provider = config.oauthProvider;
+    this.simpleOauthModule = simpleOauthModule;
+    this.oauth2 = this._getOauthClient();
+  }
+
+  _getOauthClient() {
+    return this.simpleOauthModule.create({
+      client: {
+        id: config.clientId,
+        secret: config.clientSecret
+      },
+      auth: {
+        // Supply GIT_HOSTNAME for enterprise github installs.
+        tokenHost: config.gitHostname,
+        tokenPath: config.tokenPath,
+        authorizePath: config.authorizePath
+      }
+    });
+  }
+
+  getAuthorizationUri() {
+    return this.oauth2.authorizationCode.authorizeURL({
+      redirect_uri: config.redirectUrl,
+      scope: config.scope,
+      state: randomstring.generate(32)
+    });
+  }
+
+  authorize(code) {
+    let options = { code };
+
+    if (this.oauth_provider === 'gitlab') {
+      options.client_id = process.env.OAUTH_CLIENT_ID;
+      options.client_secret = process.env.OAUTH_CLIENT_SECRET;
+      options.grant_type = 'authorization_code';
+      options.redirect_uri = process.env.REDIRECT_URL;
+    }
+
+    return oauth2.authorizationCode.getToken(options, (error, result) => {
+      let mess, content;
+
+      if (error) {
+        logger.error('Access Token Error', error.message);
+        mess = 'error';
+        content = JSON.stringify(error);
+      } else {
+        const token = oauth2.accessToken.create(result);
+        mess = 'success';
+        content = {
+          token: token.token.access_token,
+          provider: this.oauth_provider
+        };
+      }
+
+      const successScript = `
+        <script>
+        (function() {
+          function recieveMessage(e) {
+            console.log("recieveMessage %o", e)
+            // send message to main window with da router
+            window.opener.postMessage(
+              'authorization:${this.oauth_provider}:${mess}:${JSON.stringify(content)}',
+              e.origin
+            )
+          }
+          window.addEventListener("message", recieveMessage, false)
+          // Start handshare with parent
+          console.log("Sending message: %o", "${this.oauth_provider}")
+          window.opener.postMessage("authorizing:${this.oauth_provider}", "*")
+          })()
+        </script>`;
+
+      return successScript;
+    });
+  }
+}
+
+module.exports = new AuthService();
