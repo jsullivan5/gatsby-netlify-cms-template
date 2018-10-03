d44d28a070d2f31123e4f648531d59fc195c2828
James Sullivan <james_sullivan@mckinsey.com>
2018-10-02 20:10:50 -0600

    Add basic server app

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public/')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(8080, () => {
+  console.log(`PE server is listening on port ${8080}`);
+});
